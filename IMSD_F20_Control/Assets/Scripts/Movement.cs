0e76b5dfaa47672f06e93718aa3df1e16b12f9b2
Isaias <Isaias.Acosta@mail.citytech.cuny.edu>
2020-09-02 13:38:58 -0400

    made movement script

---
+++ IMSD_F20_Control/Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Transform myTransform;
+
+    public float speed;
+    void Start()
+    {
+        myTransform.position = new Vector3(-10, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTransform.position =
+            myTransform.position + new Vector3(1, 0, 0) * speed * Time.deltaTime;
+    }
+}
