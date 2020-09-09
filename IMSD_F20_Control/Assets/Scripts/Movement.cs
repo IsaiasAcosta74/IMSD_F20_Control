d5469d7d170c1e53c1576e4738822da6fce6f485
Isaias <Isaias.Acosta@mail.citytech.cuny.edu>
2020-09-08 20:13:13 -0400

    made changes

--- IMSD_F20_Control/Assets/Scripts/Movement.cs
+++ IMSD_F20_Control/Assets/Scripts/Movement.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -7,15 +9,25 @@ public class Movement : MonoBehaviour
     public Transform myTransform;
 
     public float speed;
+
+    private float timer;
     void Start()
     {
         myTransform.position = new Vector3(-10, 0, 0);
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        myTransform.position =
-            myTransform.position + new Vector3(1, 0, 0) * speed * Time.deltaTime;
+        // myTransform.position =
+        // myTransform.position + new Vector3(1, 0, 0) * speed * Time.deltaTime;
+
+        timer += Time.deltaTime;
+        if(timer >= 1)
+        {
+            timer = 0;
+            myTransform.position += new Vector3(1, 0, 0);
+        }
     }
 }
